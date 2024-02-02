f062c70c6a25f11cadde82fc7fcac4718253548d
Amar singh <asingh58885@gmail.com>
2024-02-02 21:49:16 +0530

    file structure is ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import BrowserRouter from 'react-dom'
 
 const App = () => {
   return (
@@ -8,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
